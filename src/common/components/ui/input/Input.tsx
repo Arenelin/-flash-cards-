862cfe23da31c13ebc6d1d99fc629cf9af19a5b8
murashko <alinamurashko93@gmail.com>
2024-04-27 17:50:07 +0300

    feat: Develop

--- src/common/components/ui/input/Input.tsx
+++ src/common/components/ui/input/Input.tsx
@@ -2,8 +2,10 @@ import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
 import s from './input.module.scss'
 
-import { useIcon } from './useIcon'
+import { useInput } from './useInput'
 
+// This component represents an input field with various props such as type, value, label, icon, error message,
+// and event handlers. It uses a custom hook called useInput to handle input text events and state management.
 export enum InputType {
   'password' = 'password',
   'search' = 'search',
@@ -28,6 +30,7 @@ type Props = InputProps & Omit<ComponentPropsWithoutRef<'input'>, keyof InputPro
 
 export const Input = (props: Props) => {
   const {
+    className,
     disabled,
     errorMessage,
     iconEnd,
@@ -43,7 +46,7 @@ export const Input = (props: Props) => {
   } = props
 
   const isDisabled = !!(disabled || errorMessage) //disabled || errorMessage ? true : false
-  const [onChangeHandler, onKeyDownHandler, typePassword, IconActiveNow] = useIcon(
+  const [onChangeHandler, onKeyDownHandler, typePassword, IconToggleButton] = useInput(
     onChangeText,
     onEnter,
     isDisabled,
@@ -51,18 +54,18 @@ export const Input = (props: Props) => {
     iconEndNotActive
   )
   const isSearch = type === InputType.search
-  const styleInputWithIcon = isSearch ? `${s.input} ${s.search}` : `${s.input} ${s.password}`
-  const styleInput = iconStart || IconActiveNow ? styleInputWithIcon : `${s.input}`
-  const styleInputCommon = errorMessage ? `${styleInput} ${s.error}` : `${styleInput}`
-  const styleIconEnd = isSearch ? `${s.iconEnd} ${s.search}` : `${s.iconEnd}`
-  const styleLabel = disabled ? `${s.label} ${s.labelDisabled}` : `${s.label}`
+  const inputStyleWithIcon = `${s.input} ${isSearch ? s.search : s.password}`
+  const inputStyle = iconStart || IconToggleButton ? inputStyleWithIcon : s.input
+  const baseInputStyle = `${inputStyle} ${errorMessage && s.error}`
+  const iconEndStyle = `${s.iconEnd} ${isSearch && s.search}`
+  const labelStyle = `${s.label} ${disabled && s.labelDisabled}`
 
   return (
     <div className={s.box}>
-      {label && <label className={styleLabel}>{label}</label>}
+      {label && <label className={labelStyle}>{label}</label>}
       <div className={s.inputContainer}>
         <input
-          className={styleInputCommon}
+          className={baseInputStyle}
           disabled={disabled}
           onChange={onChangeHandler}
           onKeyDown={onKeyDownHandler}
@@ -71,14 +74,14 @@ export const Input = (props: Props) => {
           {...rest}
         />
         {iconStart && <span className={s.iconStart}>{iconStart}</span>}
-        {IconActiveNow && <span className={styleIconEnd}>{IconActiveNow}</span>}
+        {IconToggleButton && <span className={iconEndStyle}>{IconToggleButton}</span>}
         {isSearch && value?.trim() && (
-          <button className={styleIconEnd} onClick={onClearClick}>
+          <button className={iconEndStyle} onClick={onClearClick}>
             {iconEnd}
           </button>
         )}
       </div>
-      {errorMessage?.length !== 0 && <span className={s.error}>{errorMessage}</span>}
+      {!!errorMessage && <span className={s.error}>{errorMessage}</span>}
     </div>
   )
 }
--- src/common/components/ui/input/useInput.tsx
+++ src/common/components/ui/input/useInput.tsx
@@ -2,7 +2,10 @@ import { ChangeEvent, KeyboardEvent, ReactNode, useState } from 'react'
 
 import { InputType } from './Input'
 
-type UseIconReturnType =
+// This function is a custom hook called useInput that is used for managing state and handling input text events in an
+// input field.
+
+type Type =
   | [
       (e: ChangeEvent<HTMLInputElement>) => void,
       (e: KeyboardEvent<HTMLInputElement>) => void,
@@ -10,16 +13,16 @@ type UseIconReturnType =
       ReactNode,
     ]
   | [(e: ChangeEvent<HTMLInputElement>) => void, (e: KeyboardEvent<HTMLInputElement>) => void]
-export const useIcon = (
+export const useInput = (
   onChangeText: (value: string) => void,
   onEnter: (() => void) | undefined,
   disabled: boolean | undefined,
   IconActive: ReactNode,
   IconNotActive: ReactNode
-): UseIconReturnType => {
+): Type => {
   const [active, setActive] = useState(false)
 
-  const IconActiveNow = (
+  const IconToggleButton = (
     <button disabled={disabled} onClick={() => setActive(prevActive => !prevActive)}>
       {active ? IconNotActive : IconActive}{' '}
     </button>
@@ -27,18 +30,20 @@ export const useIcon = (
 
   const type = active ? InputType.text : InputType.password
 
-  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     onChangeText?.(e.currentTarget.value)
   }
-  const onKeyDownCallBack = (e: KeyboardEvent<HTMLInputElement>) => {
+  const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       onEnter?.()
     }
   }
 
+  //  In this return statement, we cannot do without a condition because our variable type has the InputType type and when
+  //  returning without a condition, the type variable conflicts with undefined.
   if (IconActive && IconNotActive) {
-    return [onChangeCallback, onKeyDownCallBack, type, IconActiveNow]
+    return [onChangeHandler, onKeyDownHandler, type, IconToggleButton]
   } else {
-    return [onChangeCallback, onKeyDownCallBack]
+    return [onChangeHandler, onKeyDownHandler]
   }
 }

cd5e6ccd828ce75ab9d01bc1734070a8b5bef4ef
ViktorChizh <victor.chizh@mail.ru>
2024-05-02 11:33:00 +0300

    feature/#30 fix: slider.stories.tsx

--- src/common/components/ui/slider/slider.stories.tsx
+++ src/common/components/ui/slider/slider.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { useState } from 'react'
+
 import { Slider } from '@/common/components/ui'
 
 const meta = {
@@ -14,7 +16,9 @@ export default meta
 type Story = StoryObj<typeof Slider>
 
 export const SliderOwn = {
-  args: {
-    value: [0, 100],
+  render() {
+    const [value, setValue] = useState([0, 100])
+
+    return <Slider onValueChange={setValue} value={value} />
   },
 } satisfies Story

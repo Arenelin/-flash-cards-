cfb55ab8520cfb664fc102ccd4d1f766bdfeeb19
ViktorChizh <victor.chizh@mail.ru>
2024-05-03 14:13:02 +0300

    Feature/#25 feat: add icons gallery to storybook

---
+++ src/assets/icons/components/_IconsGallery_.stories.tsx
@@ -0,0 +1,150 @@
+import ArrowBack from '@/assets/icons/components/ArrowBack'
+import ArrowDown from '@/assets/icons/components/ArrowDown'
+import ArrowForward from '@/assets/icons/components/ArrowForward'
+import ArrowUp from '@/assets/icons/components/ArrowUp'
+import CheckboxIcon from '@/assets/icons/components/CheckboxIcon'
+import Close from '@/assets/icons/components/Close'
+import CloseOutline from '@/assets/icons/components/CloseOutline'
+import Edit2 from '@/assets/icons/components/Edit2'
+import Edit2Outline from '@/assets/icons/components/Edit2Outline'
+import Eye from '@/assets/icons/components/Eye'
+import EyeOff from '@/assets/icons/components/EyeOff'
+import EyeOffOutline from '@/assets/icons/components/EyeOffOutline'
+import EyeOutline from '@/assets/icons/components/EyeOutline'
+import Image from '@/assets/icons/components/Image'
+import ImageOutline from '@/assets/icons/components/ImageOutline'
+import LogOut from '@/assets/icons/components/LogOut'
+import LogOutOutline from '@/assets/icons/components/LogOutOutline'
+import MoreVerticalOutline from '@/assets/icons/components/MoreVerticalOutline'
+import Person from '@/assets/icons/components/Person'
+import PersonOutline from '@/assets/icons/components/PersonOutline'
+import PlayCircle from '@/assets/icons/components/PlayCircle'
+import PlayCircleOutline from '@/assets/icons/components/PlayCircleOutline'
+import Search from '@/assets/icons/components/Search'
+import SearchOutline from '@/assets/icons/components/SearchOutline'
+import Star from '@/assets/icons/components/Star'
+import StarOutline from '@/assets/icons/components/StarOutline'
+import Trash from '@/assets/icons/components/Trash'
+import TrashOutline from '@/assets/icons/components/TrashOutline'
+import { Meta } from '@storybook/react'
+
+export default {
+  title: 'Icons Gallery',
+} as Meta
+
+export const IconsGallery = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <ArrowBack />
+      <p style={{ margin: '0' }}>&nbsp; ArrowBack</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <ArrowDown />
+      <p style={{ margin: '0' }}>ArrowDown</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <ArrowForward />
+      <p style={{ margin: '0' }}>&nbsp; ArrowForward</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <ArrowUp />
+      <p style={{ margin: '0' }}>ArrowUp</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <CheckboxIcon />
+      <p style={{ margin: '0' }}>&nbsp; CheckboxIcon</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Close />
+      <p style={{ margin: '0' }}>Close</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <CloseOutline />
+      <p style={{ margin: '0' }}>CloseOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Edit2 />
+      <p style={{ margin: '0' }}>Edit2</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Edit2Outline />
+      <p style={{ margin: '0' }}>Edit2Outline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Eye />
+      <p style={{ margin: '0' }}>Eye</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <EyeOff />
+      <p style={{ margin: '0' }}>EyeOff</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <EyeOffOutline />
+      <p style={{ margin: '0' }}>EyeOffOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <EyeOutline />
+      <p style={{ margin: '0' }}>EyeOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Image />
+      <p style={{ margin: '0' }}>Image</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <ImageOutline />
+      <p style={{ margin: '0' }}>ImageOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <LogOut />
+      <p style={{ margin: '0' }}>LogOut</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <LogOutOutline />
+      <p style={{ margin: '0' }}>LogOutOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <MoreVerticalOutline />
+      <p style={{ margin: '0' }}>MoreVerticalOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Person />
+      <p style={{ margin: '0' }}>ArrowPersonBack</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <PersonOutline />
+      <p style={{ margin: '0' }}>PersonOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <PlayCircle />
+      <p style={{ margin: '0' }}>PlayCircle</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <PlayCircleOutline />
+      <p style={{ margin: '0' }}>PlayCircleOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Search />
+      <p style={{ margin: '0' }}>Search</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <SearchOutline />
+      <p style={{ margin: '0' }}>SearchOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Star />
+      <p style={{ margin: '0' }}>Star</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <StarOutline />
+      <p style={{ margin: '0' }}>StarOutline</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <Trash />
+      <p style={{ margin: '0' }}>Trash</p>
+    </div>
+    <div style={{ display: 'flex', gap: '25px' }}>
+      <TrashOutline />
+      <p style={{ margin: '0' }}>TrashOutline</p>
+    </div>
+  </div>
+)

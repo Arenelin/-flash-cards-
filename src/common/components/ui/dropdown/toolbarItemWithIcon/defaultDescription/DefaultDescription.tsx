05a6b17473d294a71a7b6ce0f5f5cd2d43683416
Arenelin <moroznaya2002@gmail.com>
2024-05-03 09:51:58 +0300

    feat: Develop components/UI/Dropdown

---
+++ src/common/components/ui/dropdown/toolbarItemWithIcon/defaultDescription/DefaultDescription.tsx
@@ -0,0 +1,16 @@
+import { Typography } from '@/common/components'
+
+import s from './defaultDescription.module.scss'
+
+type Props = {
+  text: string
+}
+export const DefaultDescription = (props: Props) => {
+  const { text } = props
+
+  return (
+    <Typography as={'span'} className={s.text} variant={'caption'}>
+      {text}
+    </Typography>
+  )
+}

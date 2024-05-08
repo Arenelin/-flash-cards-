4b694246bd1d897f8daf681752736694842f3294
ViktorChizh <victor.chizh@mail.ru>
2024-05-08 11:14:57 +0300

    Feature/#43 feat: add ref to the Button component

--- src/common/components/ui/button/Button.tsx
+++ src/common/components/ui/button/Button.tsx
@@ -1,24 +1,36 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ComponentPropsWithRef, ComponentPropsWithoutRef, ElementType, forwardRef } from 'react'
+
+import classNames from 'classnames'
 
 import s from './button.module.scss'
 
 type ButtonVariant = 'primary' | 'secondary'
+
+type PolymorphicRef<T extends ElementType> = ComponentPropsWithRef<T>['ref']
 type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
   fullWidth?: boolean
   variant?: ButtonVariant
 } & ComponentPropsWithoutRef<T>
-export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const {
-    as: Component = 'button',
-    className,
-    fullWidth = false,
-    variant = 'primary',
-    ...rest
-  } = props
-  const classNames = {
-    root: `${s.button} ${className} ${s[variant]} ${fullWidth ? s.fullWidth : ''}`,
-  }
 
-  return <Component className={classNames.root} {...rest} />
-}
+export const Button = forwardRef(
+  <T extends ElementType = 'button'>(props: ButtonProps<T>, ref: PolymorphicRef<T>) => {
+    const {
+      as: Component = 'button',
+      className,
+      fullWidth = false,
+      variant = 'primary',
+      ...rest
+    } = props
+
+    return (
+      <Component
+        className={classNames(s.button, s[variant], fullWidth && s.fullWidth, className)}
+        ref={ref}
+        {...rest}
+      />
+    )
+  }
+)
+
+Button.displayName = 'Button'

856cda8037848447d10ceea1c73a9a2ff09a952e
ViktorChizh <victor.chizh@mail.ru>
2024-05-08 12:24:55 +0300

    Feature/#43 feat: add ref to the UserAvatar component

--- src/common/components/ui/userAvatar/UserAvatar.tsx
+++ src/common/components/ui/userAvatar/UserAvatar.tsx
@@ -1,16 +1,17 @@
+import { forwardRef } from 'react'
+
 import s from './userAvatar.module.scss'
 
 type Props = {
   name?: string
   src?: string
 }
-export const UserAvatar = (props: Props) => {
-  const { name = '', src } = props
+export const UserAvatar = forwardRef<HTMLDivElement, Props>(({ name = '', src }, ref) => {
   const imgSrc = src ? src : `https://ui-avatars.com/api/?name=${name}`
 
   return (
-    <div className={s.container}>
+    <div className={s.container} ref={ref}>
       <img alt={'User photo'} className={s.img} src={imgSrc} />
     </div>
   )
-}
+})

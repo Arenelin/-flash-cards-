fc2b37f38efef542ed1ac4542c5d9dac7945ecce
murashko <alinamurashko93@gmail.com>
2024-05-08 14:42:24 +0300

    feature/#62 feat:Develop Controlled-input and storybook

--- src/common/components/ui/controlled/controlled-input/control-input.stories.tsx
+++ src/common/components/ui/controlled/controlled-input/control-input.stories.tsx
@@ -2,38 +2,57 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { useForm } from 'react-hook-form'
 
+import EyeOffOutline from '@/assets/icons/EyeOffOutline'
+import EyeOutline from '@/assets/icons/EyeOutline'
 import { InputType } from '@/common/components/ui'
 
 import { ControlledInput } from './Controlled-input'
 
 const meta = {
-  argTypes: {},
   component: ControlledInput,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  title: 'Components/Input',
+  title: 'Components/ControlledInput',
 } satisfies Meta<typeof ControlledInput>
 
 export default meta
 type Story = StoryObj<typeof ControlledInput>
 
-export const TextInput = {
+type FormType = {
+  email: string
+  password: string
+}
+export const ControlledInputs = {
   render: () => {
-    const { handleSubmit, register } = useForm()
+    const { control, handleSubmit } = useForm<FormType>()
     const onSubmit = (data: any) => {
       console.log(data)
     }
 
     return (
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}
+      >
         <ControlledInput
-          {...register('email')}
-          label={'Input'}
-          placeholder={'Input'}
+          control={control}
+          label={'Email'}
+          name={'email'}
+          placeholder={'email'}
           type={InputType.text}
         />
+        <ControlledInput
+          control={control}
+          iconEnd={<EyeOffOutline />}
+          iconEndNotActive={<EyeOutline />}
+          label={'Password'}
+          name={'password'}
+          placeholder={'password'}
+          type={InputType.password}
+        />
+        <button type={'submit'}>Submit</button>
       </form>
     )
   },

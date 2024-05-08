be7160e5e957ee505fb0abcdc8e496c3f283e749
murashko <alinamurashko93@gmail.com>
2024-05-08 10:55:37 +0300

    fix: Replace forward ref typing for a polymorphic component

--- src/common/components/ui/card/Card.tsx
+++ src/common/components/ui/card/Card.tsx
@@ -1,48 +1,22 @@
-import {
-  ComponentPropsWithRef,
-  ComponentPropsWithoutRef,
-  ElementType,
-  ReactNode,
-  forwardRef,
-  useId,
-} from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ElementType, ForwardedRef, forwardRef } from 'react'
 
 import classNames from 'classnames'
 
 import s from '@/common/components/ui/card/card.module.scss'
 
-//https://www.benmvp.com/blog/forwarding-refs-polymorphic-react-component-typescript/
-//https://www.freecodecamp.org/news/build-strongly-typed-polymorphic-components-with-react-and-typescript/
-
-type Variant = 'article' | 'aside' | 'div' | 'section'
-
 type CardProps<T extends ElementType = 'div'> = {
-  as?: T | Variant
-} & { ref?: PolymorphicRef<T> } & ComponentPropsWithoutRef<T>
+  as?: T
+} & ComponentPropsWithoutRef<T>
 
-type Props<T extends ElementType = 'div'> = CardProps<T> &
-  Omit<ComponentPropsWithoutRef<T>, keyof CardProps<T>>
-
-type PolymorphicRef<T extends ElementType> = ComponentPropsWithRef<T>['ref']
-
-type CardWithRef = <T extends ElementType = 'div'>(
-  props: CardProps<T>,
-  ref?: PolymorphicRef<T>
-) => ReactNode
-
-export const Card: CardWithRef = forwardRef(function Card<T extends ElementType = 'div'>(
-  props: Props<T>,
-  ref?: PolymorphicRef<T>
-) {
+const CardPolymorph = <T extends ElementType = 'div'>(props: CardProps<T>, ref?: any) => {
   const { as: Component = 'div', className, id, ...rest } = props
 
-  const genID = useId()
-  const finalId = id || genID
-  const baseClassName = {
-    container: classNames(s.container, className),
-  }
+  return <Component className={classNames(s.container, className)} {...rest} ref={ref}></Component>
+}
 
-  return (
-    <Component className={baseClassName.container} ref={ref} {...rest} id={finalId}></Component>
-  )
-})
+export const Card = forwardRef(CardPolymorph) as <T extends ElementType = 'button'>(
+  props: {
+    ref?: ForwardedRef<ElementRef<T>>
+  } & CardProps<T> &
+    Omit<ComponentPropsWithoutRef<T>, keyof CardProps<T>>
+) => ReturnType<typeof CardPolymorph>

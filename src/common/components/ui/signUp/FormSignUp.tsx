13d87367c63385cf15d218dde6d0460d2b8ce2c0
murashko <alinamurashko93@gmail.com>
2024-05-09 21:23:45 +0300

    feature/#76 fix: Remove the type in the button and the as attribute

--- src/common/components/ui/signUp/FormSignUp.tsx
+++ src/common/components/ui/signUp/FormSignUp.tsx
@@ -4,7 +4,6 @@ import { useForm } from 'react-hook-form'
 import { EyeOffOutline, EyeOutline } from '@/assets/icons'
 import { Button, Card, InputType, Typography } from '@/common/components/ui'
 import { ControlledInput } from '@/common/components/ui/controlled/controlled-input/Controlled-input'
-import { DevTool } from '@hookform/devtools'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
@@ -47,55 +46,51 @@ export const FormSignUp = ({ onSubmit }: Props) => {
   const formId = useId()
 
   return (
-    <>
-      <DevTool control={control} />
-
-      <Card className={s.card}>
-        <Typography as={'h1'} variant={'h1'}>
+    <Card className={s.card}>
+      <Typography as={'h1'} variant={'h1'}>
+        Sign Up
+      </Typography>
+      <form className={s.form} id={formId} onSubmit={handleSubmit(onSubmit)}>
+        <div className={s.containerInput}>
+          <ControlledInput
+            className={s.input}
+            control={control}
+            label={'Email'}
+            name={'email'}
+            placeholder={'email'}
+            type={InputType.text}
+          />
+          <ControlledInput
+            className={s.input}
+            control={control}
+            iconEnd={<EyeOffOutline />}
+            iconEndNotActive={<EyeOutline />}
+            label={'Password'}
+            name={'password'}
+            placeholder={'password'}
+            type={InputType.password}
+          />
+          <ControlledInput
+            className={s.input}
+            control={control}
+            iconEnd={<EyeOffOutline />}
+            iconEndNotActive={<EyeOutline />}
+            label={'Confirm password'}
+            name={'confirmPassword'}
+            placeholder={'confirm password'}
+            type={InputType.password}
+          />
+        </div>
+        <Button className={s.button} form={formId} fullWidth>
           Sign Up
-        </Typography>
-        <form className={s.form} id={formId} onSubmit={handleSubmit(onSubmit)}>
-          <div className={s.containerInput}>
-            <ControlledInput
-              className={s.input}
-              control={control}
-              label={'Email'}
-              name={'email'}
-              placeholder={'email'}
-              type={InputType.text}
-            />
-            <ControlledInput
-              className={s.input}
-              control={control}
-              iconEnd={<EyeOffOutline />}
-              iconEndNotActive={<EyeOutline />}
-              label={'Password'}
-              name={'password'}
-              placeholder={'password'}
-              type={InputType.password}
-            />
-            <ControlledInput
-              className={s.input}
-              control={control}
-              iconEnd={<EyeOffOutline />}
-              iconEndNotActive={<EyeOutline />}
-              label={'Confirm password'}
-              name={'confirmPassword'}
-              placeholder={'confirm password'}
-              type={InputType.password}
-            />
-          </div>
-          <Button as={'button'} className={s.button} form={formId} fullWidth type={'submit'}>
-            Sign Up
-          </Button>
-        </form>
-        <Typography as={'h2'} variant={'body2'}>
-          Already have an account?
-        </Typography>
-        <Typography as={'a'} className={s.signIn} variant={'h4'}>
-          Sign In
-        </Typography>
-      </Card>
-    </>
+        </Button>
+      </form>
+      <Typography as={'h2'} variant={'body2'}>
+        Already have an account?
+      </Typography>
+      <Typography as={'a'} className={s.signIn} href={'#'} variant={'link3'}>
+        Sign In
+      </Typography>
+    </Card>
   )
 }

8cd476b3d5f715196382880eedb9f796602cb376
murashko <alinamurashko93@gmail.com>
2024-05-13 14:55:54 +0300

    feat: Add route schemes,BrowserRouter

---
+++ src/app/router.tsx
@@ -0,0 +1,69 @@
+// @ts-ignore
+import {
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
+
+import { Page } from '@/common/components/ui/page/Page'
+
+import { FormForgotPassword } from '../features/forms/forgotPassword/FormForgotPassword'
+import { FormSignIn } from '../features/forms/signIn/FormSignIn'
+import { FormSignUp } from '../features/forms/signUp/FormSignUp'
+
+const onSubmit = () => console.log('rout')
+
+enum path {
+  forgotPassword = 'forgot-password',
+  signIn = 'signIn',
+  signUp = 'signUp',
+}
+
+const publicRoutes: RouteObject[] = [
+  {
+    element: (
+      <Page>
+        <FormForgotPassword onSubmit={onSubmit} />
+      </Page>
+    ),
+    path: path.forgotPassword,
+  },
+  {
+    element: (
+      <Page>
+        <FormSignIn onSubmit={onSubmit} />
+      </Page>
+    ),
+    path: path.signIn,
+  },
+
+  {
+    element: (
+      <Page>
+        <FormSignUp onSubmit={onSubmit} />
+      </Page>
+    ),
+    path: path.signUp,
+  },
+]
+
+const privateRoutes = [{ element: <div>desks</div>, path: '/desks' }]
+const PrivateRouter = () => {
+  const isAuthenticated = false
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
+}
+
+const router = createBrowserRouter([
+  {
+    children: privateRoutes,
+    element: <PrivateRouter />,
+  },
+  ...publicRoutes,
+])
+
+export function Router() {
+  return <RouterProvider router={router} />
+}

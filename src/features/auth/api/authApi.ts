219c6c3ae69d0630cfe14068e97cff131e5c9f3a
murashko <alinamurashko93@gmail.com>
2024-05-16 14:48:50 +0300

    feature/#111 feat: Add authApi, add types

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,14 +1,79 @@
 import { appApi } from '@/app/api/appApi'
+import {
+  ForgotPasswordArgs,
+  MeArgs,
+  MeResponse,
+  ResetPasswordTokenArgs,
+  SignInArgs,
+  SignInResponse,
+  SignUpArgs,
+  SignUpResponse,
+} from '@/common/types'
 
 export const authApi = appApi.injectEndpoints({
-  endpoints: () => ({}),
-  // endpoints: builder => {
-  //   return {
-  //     getDecks: builder.query<any, void>({
-  //       query: () => `v2/decks`,
-  //     }),
-  //   }
-  // },
+  endpoints: builder => {
+    return {
+      authToken: builder.query<undefined, void>({
+        query: () => ({
+          method: 'POST',
+          url: 'v2/auth/refresh-token',
+        }),
+      }),
+      forgotPassword: builder.query<undefined, ForgotPasswordArgs>({
+        query: body => ({
+          body: body,
+          method: 'POST',
+          url: 'v1/auth/recover-password',
+        }),
+      }),
+      getMe: builder.query<MeResponse, void>({
+        query: () => 'v1/auth/me',
+      }),
+      logOut: builder.query<undefined, void>({
+        query: () => ({
+          method: 'POST',
+          url: 'v1/auth/logout',
+        }),
+      }),
+      resetPasswordToken: builder.mutation<undefined, ResetPasswordTokenArgs>({
+        query: ({ token, ...body }) => ({
+          method: 'POST',
+          params: body,
+          url: `v1/auth/reset-password/${token}`,
+        }),
+      }),
+      signIn: builder.query<SignInResponse, SignInArgs>({
+        query: body => ({
+          body: body,
+          method: 'POST',
+          url: 'v1/auth/login',
+        }),
+      }),
+      signUp: builder.mutation<SignUpResponse, SignUpArgs>({
+        query: body => ({
+          body: body,
+          method: 'POST',
+          url: 'v1/auth/sign-up',
+        }),
+      }),
+      updateMe: builder.mutation<MeResponse, MeArgs>({
+        query: body => ({
+          body: body,
+          method: 'PATCH',
+          url: 'v1/auth/me',
+        }),
+      }),
+    }
+  },
 })
 
-export const {} = authApi
+export const {
+  useAuthTokenQuery,
+  useForgotPasswordQuery,
+  useGetMeQuery,
+  useLogOutQuery,
+  useResetPasswordTokenMutation,
+  useSignInQuery,
+  useSignUpMutation,
+  useUpdateMeMutation,
+} = authApi

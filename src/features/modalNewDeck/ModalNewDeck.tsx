6145e518641fbe41e603e523ae74ff8a25538c00
murashko <alinamurashko93@gmail.com>
2024-05-17 18:24:14 +0300

    feature/#122 add Style modalNewDeck

--- src/features/modalNewDeck/ModalNewDeck.tsx
+++ src/features/modalNewDeck/ModalNewDeck.tsx
@@ -8,6 +8,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { z } from 'zod'
 
+import s from './modalNewDeck.module.scss'
+
 type ModalProps = {
   onOpenChange: (open: boolean) => void
   onSubmit: (data: any) => void
@@ -29,18 +31,27 @@ export const ModalNewDeck = forwardRef<ElementRef<typeof DialogPrimitive.Content
 
     return (
       <Modal onOpenChange={onOpenChange} open={open} ref={ref} title={title}>
-        <form id={finalId} onSubmit={handleSubmit(onSubmit)}>
-          <ControlledInput control={control} name={'name'} type={InputType.text} />
+        <form className={s.container} id={finalId} onSubmit={handleSubmit(onSubmit)}>
+          <ControlledInput
+            className={s.input}
+            control={control}
+            label={'Name Pack'}
+            name={'name'}
+            placeholder={'Name'}
+            type={InputType.text}
+          />
           <Button fullWidth variant={'secondary'}>
             <ImageOutline /> Upload Image
           </Button>
           <ControlledCheckbox control={control} label={'Private pack'} name={'private'} />
-          <Button onClick={() => onOpenChange(false)} type={'button'} variant={'secondary'}>
-            Cancel
-          </Button>
-          <Button form={finalId} variant={'primary'}>
-            Add New Pack
-          </Button>
+          <div className={s.containerButton}>
+            <Button onClick={() => onOpenChange(false)} type={'button'} variant={'secondary'}>
+              Cancel
+            </Button>
+            <Button form={finalId} variant={'primary'}>
+              Add New Pack
+            </Button>
+          </div>
         </form>
       </Modal>
     )

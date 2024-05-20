755eb88b25db1bafed081f89603c0f27f5c33104
murashko <alinamurashko93@gmail.com>
2024-05-20 16:14:03 +0300

    feature/#122 MODAL DECK

--- src/features/modals/modalEditForm/ModalDeck.tsx
+++ src/features/modals/modalEditForm/ModalDeck.tsx
@@ -3,16 +3,23 @@ import { useForm } from 'react-hook-form'
 
 import { Button, InputType, Modal } from '@/common/components'
 import { ControlledCheckbox, ControlledInput } from '@/common/components/controlled'
-import { ControlledInputFile } from '@/features/modals/modalAddEditForm/ControlledInputFile'
+import { ControlledInputFile } from '@/features/modals/ui/ControlledInputFile'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { z } from 'zod'
 
-import s from './ModalAddEditForm.module.scss'
+import s from './modalDeck.module.scss'
+
+type EditData = {
+  cover: null | string | undefined
+  name: string
+  private: boolean
+}
 
 type ModalProps = {
+  data?: EditData
   onOpenChange: (open: boolean) => void
-  onSubmit: (data: NewDeck) => void
+  onSubmit: (data: EditDeck) => void
   open: boolean
   title: string
 } & Omit<ComponentPropsWithoutRef<typeof DialogPrimitive.Root>, 'onOpenChange' | 'open'>
@@ -22,22 +29,30 @@ const schemaFile = z
   .refine(file => file.size < 1000000, {
     message: 'Your resume must be less than 1MB.',
   })
+  .nullish()
   .optional()
 
 const newDeckSchema = z.object({
-  file: schemaFile,
+  cover: z.union([schemaFile, z.string()]),
   name: z.string().min(1).max(50),
   private: z.boolean().optional(),
 })
 
-export type NewDeck = z.infer<typeof newDeckSchema>
-export const ModalAddEditForm = forwardRef<ElementRef<typeof DialogPrimitive.Content>, ModalProps>(
+export type EditDeck = z.infer<typeof newDeckSchema>
+export const ModalDeck = forwardRef<ElementRef<typeof DialogPrimitive.Content>, ModalProps>(
   (props, ref) => {
-    const { onOpenChange, onSubmit, open, title } = props
-    const { control, handleSubmit } = useForm<NewDeck>({ resolver: zodResolver(newDeckSchema) })
+    const { data, onOpenChange, onSubmit, open, title } = props
+    const { control, handleSubmit } = useForm<EditDeck>({
+      defaultValues: {
+        cover: data?.cover,
+        name: data?.name,
+        private: data?.private,
+      },
+      resolver: zodResolver(newDeckSchema),
+    })
     const finalId = useId()
 
-    const onHandleSubmit = (data: NewDeck) => {
+    const onHandleSubmit = (data: EditDeck) => {
       onSubmit(data)
       onOpenChange(false)
     }
@@ -53,7 +68,7 @@ export const ModalAddEditForm = forwardRef<ElementRef<typeof DialogPrimitive.Con
             placeholder={'Name'}
             type={InputType.text}
           />
-          <ControlledInputFile control={control} name={'file'} />
+          <ControlledInputFile control={control} name={'cover'} />
           <ControlledCheckbox control={control} label={'Private pack'} name={'private'} />
           <div className={s.containerButton}>
             <Button onClick={() => onOpenChange(false)} type={'button'} variant={'secondary'}>
@@ -68,3 +83,5 @@ export const ModalAddEditForm = forwardRef<ElementRef<typeof DialogPrimitive.Con
     )
   }
 )
+
+ModalDeck.displayName = 'ModalDeck'

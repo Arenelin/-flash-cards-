e4af91a9cbe36d8bfe30124dcb27fd4107b49aae
ViktorChizh <victor.chizh@mail.ru>
2024-05-22 10:38:13 +0300

    feature/#132 feat: add meRequest at FormSignIn component

--- src/features/auth/ui/signIn/FormSignIn.tsx
+++ src/features/auth/ui/signIn/FormSignIn.tsx
@@ -1,6 +1,6 @@
 import { useId } from 'react'
 import { useForm } from 'react-hook-form'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, Navigate } from 'react-router-dom'
 
 import EyeOffOutline from '@/assets/icons/EyeOffOutline'
 import EyeOutline from '@/assets/icons/EyeOutline'
@@ -8,6 +8,7 @@ import { Button, Card, InputType, Typography } from '@/common/components'
 import { ControlledCheckbox, ControlledInput } from '@/common/components/controlled'
 import { path } from '@/common/enums'
 import { emailSchema, passwordSchema } from '@/common/utils/zodSchema'
+import { useGetMeQuery } from '@/features/auth/api/authApi'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
@@ -27,6 +28,12 @@ export type SignIn = z.infer<typeof loginSchema>
 export const FormSignIn = ({ onSubmit }: Props) => {
   const { control, handleSubmit } = useForm<SignIn>({ resolver: zodResolver(loginSchema) })
   const formId = useId()
+
+  const { data: meData } = useGetMeQuery()
+
+  if (meData?.id) {
+    return <Navigate to={path.decks} />
+  }
 
   return (
     <Card className={s.card}>
